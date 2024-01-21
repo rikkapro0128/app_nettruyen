aaf6c650b6df01853211350254ffe6880e46325e
rikkapro0128 <stkssc002@gmail.com>
2024-01-21 21:26:46 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.nettruyen',
+  appName: 'nettruyen',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
